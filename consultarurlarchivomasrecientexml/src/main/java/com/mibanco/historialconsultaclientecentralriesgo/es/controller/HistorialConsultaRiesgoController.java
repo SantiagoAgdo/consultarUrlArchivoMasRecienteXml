ead93821ca73b1d95136260295adc568567d61a5
santiagoagudelo <agudelo.santigo@gmail.com>
2023-09-19 14:56:41 -0500

    Init and complete

---
+++ consultarurlarchivomasrecientexml/src/main/java/com/mibanco/historialconsultaclientecentralriesgo/es/controller/HistorialConsultaRiesgoController.java
@@ -0,0 +1,12 @@
+package com.mibanco.historialconsultaclientecentralriesgo.es.controller;
+
+import com.mibanco.historialconsultaclientecentralriesgo.es.gen.contract.V1Historialconsultaclientecentralriesgo;
+import com.mibanco.historialconsultaclientecentralriesgo.es.gen.type.TipoDocumentoEnum;
+import jakarta.ws.rs.core.Response;
+
+public class HistorialConsultaRiesgoController implements V1Historialconsultaclientecentralriesgo {
+    @Override
+    public Response consultarUrlArchivoMasRecienteXml(TipoDocumentoEnum tipoDocumento, Integer numeroDocumento, Integer digitoVerificacion) {
+        return Response.status(Response.Status.OK).entity("https://www.pruebaconsultaurl.com").build();
+    }
+}
